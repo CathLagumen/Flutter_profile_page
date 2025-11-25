8462b58c9da1f0acb2cd4956e54fbcef66af6a85
Ilagan Cathlene (External) <cathlene.ilagan@betterask.erni>
2025-11-25 13:21:41 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Profile',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const ProfilePage(),
+    );
+  }
+}
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Profile'),
+      ),
+      body:  
+      CircleAvatar(
+        radius: 60,
+        backgroundColor: const Color.fromARGB(204, 219, 223, 167),
+        child: Icon(Icons.person, size: 60, color: Colors.white),
+      )
+    );
+  }
+}
