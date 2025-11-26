b69aa3b7fd0c1b782cb75a15cc61bd7a2cb94879
Ilagan Cathlene (External) <cathlene.ilagan@betterask.erni>
2025-11-26 17:49:43 +0800

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,31 +11,115 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My Profile',
+      title: 'Profile Page',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(227, 146, 34, 146)),
         useMaterial3: true,
       ),
-      home: const ProfilePage(),
+      home: const HomePage(),
     );
   }
 }
 
-class ProfilePage extends StatelessWidget {
-  const ProfilePage({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Profile Page'),
+      ),
+
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/butterfly1.jpg'),
+            fit: BoxFit.cover,
+          ),
+      ),
+
+
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // if (_showImage)
+            const CircleAvatar(
+              radius: 100,
+              backgroundImage: AssetImage('assets/images/20240314_Lisa_Manoban_07.jpg'),
+            ),        
+    
+            const SizedBox(height: 20),
+
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(179, 18, 16, 16),
+                borderRadius: BorderRadius.circular(10),
+            ),
+              child:
+            const Text(
+              'Welcome to My Profile Page!',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Color.fromARGB(255, 239, 233, 233), fontFamily: 'Poppins',),
+            ),
+            ),
+          
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => AboutMePage()),
+                );
+              },
+
+              child: Text('About Me'),
+              
+
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+class AboutMePage extends StatelessWidget {
+  const AboutMePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('My Profile'),
+        title: const Text("About Me"),
       ),
-      body:  
-      CircleAvatar(
-        radius: 60,
-        backgroundColor: const Color.fromARGB(204, 219, 223, 167),
-        child: Icon(Icons.person, size: 60, color: Colors.white),
-      )
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: GridView.count(
+          crossAxisCount: 2,
+          crossAxisSpacing: 12,
+          mainAxisSpacing: 12,
+          children: [
+            _buildBox(
+              title: "Basic Info",
+              icon: Icons.person,
+              content: "Name: Your Name\nAge: 25\nLocation: Philippines",
+            ),
+            _buildBox(
+              title: "Contact Details",
+              icon: Icons.phone,
+              content: "Email: you@email.com\nPhone: 09xx-xxx-xxxx",
+            )
+          ],
+        ),
+      ),
     );
   }
 }
